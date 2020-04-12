a59daeb12de47b6fbeb0e04f67ddca401e236f97
Misael Cureño <curenosm@gmail.com>
2020-04-12 14:02:50 -0500

    Script adds file to .bashrc

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 
 DIR=~/scripts
 
-wget https://raw.github.com/django/django/master/extras/django_bash_completion -p $DIR
+mkdir -p $DIR
+
+wget https://raw.github.com/django/django/master/extras/django_bash_completion -P $DIR && \
+echo -e '\n\n# DJANGO BASH COMPLETION\n[ -s "$DIR/django_bash_completion" ] && \\. "$DIR/django_bash_completion"' >> ~/.bashrc
 
