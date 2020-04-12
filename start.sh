eee364550a02b81c11e85edb590edadfda60490f
Misael Cureño <curenosm@gmail.com>
2020-04-12 14:40:34 -0500

    Script works for virtualenvs or systems with python3 as a main .py interpreter

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ DIR=~/scripts
 mkdir -p $DIR
 
 wget https://raw.github.com/django/django/master/extras/django_bash_completion -P $DIR && \
-echo -e '\n\n# DJANGO BASH COMPLETION\n[ -s "$DIR/django_bash_completion" ] && \\. "$DIR/django_bash_completion"' >> ~/.bashrc
+echo -e "\n\n# DJANGO BASH COMPLETION\n. $DIR/django_bash_completion" >> ~/.bashrc
+
 
